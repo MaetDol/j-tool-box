76100b7be3522db6cbd9701e5ecd8cb5404d3ffd
MaetDol <rndka9@gmail.com>
2022-08-20 04:28:25 +0900

    QnA 인코딩 해주는 페이지 완성
    와아~

--- src/pages/ProgramQna/ProgramQna.page.tsx
+++ src/pages/ProgramQna/ProgramQna.page.tsx
@@ -1,5 +1,29 @@
+import { Antd, QnaParser } from "components";
+import { QnA } from "components/QnaParser";
+import { useEffect, useState } from "react";
+
 export default function ProgramQna() {
+    const [parsedQnas, setParsedQnas] = useState<QnA[]>([]);
+    const [stringifyQnas, setStringfyQnas] = useState('');
+    useEffect(() =>{
+        setStringfyQnas(
+            JSON.stringify(parsedQnas, null, 4)
+        );
+    }, [parsedQnas]);
+
+
     return (
-        <div>프로그램 qna 에용 ㅎ</div>
+        <div>
+            <Antd.Typography.Title>
+                QnA 를 인코딩해주는 툴입니다
+            </Antd.Typography.Title>
+            <QnaParser setParsedValue={setParsedQnas}/>
+            <Antd.Input.TextArea 
+                autoSize
+                style={{ marginTop: '24px' }}
+                value={stringifyQnas}
+                onChange={e => setStringfyQnas(e.target.value)}
+            />
+        </div>
     )
 }
