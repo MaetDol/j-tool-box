637f1e197a8ea43da7dc91a36668ccbabf694a36
MaetDol <rndka9@gmail.com>
2022-08-20 02:56:51 +0900

    기본적인 App 구조 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { Breadcrumb, Layout } from 'antd';
+import 'antd/dist/antd.css';
+import { Menu } from 'components';
+import { Home } from 'pages';
+import { Route, Routes } from 'react-router-dom';
+import Routings from 'routing';
+
+const { Sider } = Layout;
+
 
 function App() {
   return (
-    <div className="App">
-      아뇽하세요
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Layout>
+        <Sider width={256}>
+          <Menu />
+        </Sider>
+        <Layout style={{ padding: '16px' }}>
+          <Breadcrumb style={{ margin: '16px 0'}}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+          </Breadcrumb>
+
+          <Layout style={{ backgroundColor: 'white', padding: '16px' }}>
+            <Routings />
+          </Layout>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 }
 
