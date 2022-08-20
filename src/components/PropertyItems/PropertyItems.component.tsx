ca25c692b9b8089a1f18301af3465e422ae5a3fe
MaetDol <rndka9@gmail.com>
2022-08-20 21:30:19 +0900

    분리한 PropertyItem 적용

--- src/components/PropertyItems/PropertyItems.component.tsx
+++ src/components/PropertyItems/PropertyItems.component.tsx
@@ -1,4 +1,4 @@
-import { Antd } from "components";
+import { PropertyItem } from "components";
 import { useEventHandlers } from "./PropertyItems.hooks";
 import { Properties } from "./PropertyItems.models";
 
@@ -17,32 +17,14 @@ export default function PropertyItems({ properties, updateProperties }: Props) {
   return (
     <>
       {Object.entries(properties).map(([id, [name, value]]) => (
-        <Antd.Input.Group
+        <PropertyItem
           key={id}
-          style={{ display: "flex", marginTop: "16px" }}
-        >
-          <div style={{ width: "40px" }} />
-          <Antd.Input
-            prefix={'{ " '}
-            suffix={' " '}
-            addonAfter=":"
-            placeholder="property name"
-            style={{ flex: "1", minWidth: "240px" }}
-            value={name}
-            onChange={getChangePropertyKeyHandler(id)}
-          />
-          <Antd.Input
-            placeholder="property value"
-            prefix={' " '}
-            suffix={' " }'}
-            style={{ flex: "3", borderLeft: 0 }}
-            value={value}
-            onChange={getChangePropertyValueHandler(id)}
-          />
-          <Antd.Button type="primary" danger onClick={deleteProperty(id)}>
-            Delete
-          </Antd.Button>
-        </Antd.Input.Group>
+          name={name}
+          value={value}
+          changeNameHandler={getChangePropertyKeyHandler(id)}
+          changeValueHandler={getChangePropertyValueHandler(id)}
+          deleteHandler={deleteProperty(id)}
+        />
       ))}
     </>
   );
