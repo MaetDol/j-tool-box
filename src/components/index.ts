c399658531a871a213a5ec8774dbe04706c7ac9c
MaetDol <rndka9@gmail.com>
2022-08-20 21:27:46 +0900

    ProertyItem 분리
    드래그앤드랍 구현할때 가독성을 위해 분리했어용

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Breadcrumb } from "./Breadcrumb";
 export { default as QnaParser } from "./QnaParser";
 export { default as ContentsBlock } from "./ContentsBlock";
 export { default as PropertyItems } from "./PropertyItems";
+export { default as PropertyItem } from "./PropertyItem";
