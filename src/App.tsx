6da25cb8b471dc3b97002f04c60fc5ae072a3fbd
MaetDol <rndka9@gmail.com>
2022-09-09 03:07:05 +0900

    사이드바 고정되게 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import 'antd/dist/antd.css';
-import { Antd, Breadcrumb, Menu } from 'components';
-import Routings from 'routing';
+import "antd/dist/antd.css";
+import { Antd, Breadcrumb, Menu } from "components";
+import Routings from "routing";
 
 const { Sider } = Antd.Layout;
 
-
 function App() {
   return (
-    <Antd.Layout style={{ minHeight: '100vh' }}>
+    <Antd.Layout style={{ minHeight: "100vh" }}>
       <Antd.Layout>
-        <Sider width={256}>
+        <Sider width={256} style={{ position: "fixed", height: "100vh" }}>
           <Menu />
         </Sider>
-        <Antd.Layout style={{ padding: '16px' }}>
-          <Breadcrumb style={{ margin: '16px'}}/>
+        <Antd.Layout style={{ padding: "16px", marginLeft: "256px" }}>
+          <Breadcrumb style={{ margin: "16px" }} />
 
-          <Antd.Layout style={{ backgroundColor: 'white', padding: '16px' }}>
+          <Antd.Layout style={{ backgroundColor: "white", padding: "16px" }}>
             <Routings />
           </Antd.Layout>
         </Antd.Layout>
