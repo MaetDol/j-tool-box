3f4a2b23973b8e2088127c8a72023215e0e3a84d
MaetDol <rndka9@gmail.com>
2022-09-09 23:20:30 +0900

    프로그램 카드에서 사용될 enum 작성

---
+++ src/pages/ProgramCard/ProgramCard.model.ts
@@ -0,0 +1,44 @@
+export enum ProgramCardType {
+  배너 = 1,
+  기획전 = 2,
+  프로그램 = 3,
+}
+
+export enum ProgramCardUIType {
+  Wide = "P001",
+  CircleMask = "P002",
+  전문쌤검정 = "P003",
+  전문쌤하양 = "P004",
+  배너기본 = "B001",
+  기획전기본 = "E001",
+  기획전기본2 = "E002",
+}
+
+export enum ProgramCardConceptType {
+  전문가 = 1,
+  일반 = 2,
+}
+
+export enum ProgramCardLinkType {
+  없음 = 0,
+  내부 = 1,
+  외부 = 2,
+}
+
+export enum ProgramCardSubjectType {
+  외국어 = 1,
+  수학과학 = 2,
+  한글국어 = 3,
+  예체능 = 4,
+}
+
+export enum ProgramCardLocationType {
+  가정방문 = 0,
+  온라인 = 1,
+  현장체험 = 2,
+}
+
+export enum ProgramCardTarget {
+  부모님 = 2,
+  아이 = 4,
+}
