f39125b7173402d0d672363c288def6e9e10fae5
MaetDol <rndka9@gmail.com>
2022-09-09 14:26:37 +0900

    메인 페이지에 타임라인 보이도록 작업!

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,19 +1,33 @@
+import { Antd } from "components";
 import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { timeline } from "./Home.model";
 
 export default function Home() {
-    const [searchParams] = useSearchParams();
-    const navigate = useNavigate();
-    useEffect(() => {
-        const redirect = searchParams.get('redirect');
-        if(!redirect) return;
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    const redirect = searchParams.get("redirect");
+    if (!redirect) return;
 
-        navigate(redirect);
-    }, [searchParams, navigate]);
+    navigate(redirect);
+  }, [searchParams, navigate]);
 
-    return (
-        <div>
-            Yay!
-        </div>
-    )
+  return (
+    <div>
+      <Antd.Typography.Title>타임 라인</Antd.Typography.Title>
+      <Antd.Timeline>
+        {timeline.map(({ timestamp, contents }) => (
+          <Antd.Timeline.Item key={timestamp}>
+            <Antd.Tag>{timestamp}</Antd.Tag>
+            {contents.map((content) => (
+              <p style={{ margin: 0 }} key={content}>
+                {content}
+              </p>
+            ))}
+          </Antd.Timeline.Item>
+        ))}
+      </Antd.Timeline>
+    </div>
+  );
 }
