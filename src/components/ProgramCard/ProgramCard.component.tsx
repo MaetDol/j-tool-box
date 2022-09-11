c3406a83f17d52fc42b7d1e6b8c3fa24604cbb84
MaetDol <rndka9@gmail.com>
2022-09-11 13:31:36 +0900

    이벤트 카드 구현!

--- src/components/ProgramCard/ProgramCard.component.tsx
+++ src/components/ProgramCard/ProgramCard.component.tsx
@@ -2,6 +2,7 @@ import {
   ProgramCardUIType,
   TagData,
 } from "pages/ProgramCardGenerator/ProgramCardGenerator.model";
+import { EventE001, EventE002 } from "./components/Event";
 import { Circle, Expert, ExpertWhite, Wide } from "./components/Program";
 
 type Props = {
@@ -17,8 +18,8 @@ const cardMap = {
   [ProgramCardUIType.CircleMask]: Circle,
   [ProgramCardUIType.전문쌤검정]: Expert,
   [ProgramCardUIType.전문쌤하양]: ExpertWhite,
-  [ProgramCardUIType.기획전기본]: undefined,
-  [ProgramCardUIType.기획전기본2]: undefined,
+  [ProgramCardUIType.기획전기본]: EventE001,
+  [ProgramCardUIType.기획전기본2]: EventE002,
   [ProgramCardUIType.배너기본]: undefined,
 } as const;
 
